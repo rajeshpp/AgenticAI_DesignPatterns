e6bff5c93fa0d869a0c2428edef3728f18002424
rajeshpp <rajesh.paleru@gmail.com>
2025-11-16 11:09:55 +0530

    Initial

--- ReAct/Weather_Version1/check_weather.py
+++ ReAct/Weather_Version1/check_weather.py
@@ -1,7 +1,7 @@
 from random import choice
 
 def get_weather(city):
-    weather_data = {"Delhi": "Sunny, 31°C", "Bangalore": "Rainy, 25°C", "Mumbai": "Cloudy, 29°C"}
+    weather_data = {"Hyderabad": "Sunny, 32°C", "Delhi": "Sunny, 31°C", "Bangalore": "Rainy, 25°C", "Mumbai": "Cloudy, 29°C"}
     return weather_data.get(city, "Unknown")
 
 def react_agent(user_input):
@@ -13,11 +13,11 @@ def react_agent(user_input):
         print("Reasoning:", reasoning)
 
         # Act
-        observation = get_weather("Delhi")
+        observation = get_weather("Hyderabad")
         print("Action: Fetch weather → Observation:", observation)
 
         # Reason again
-        final_response = f"The weather in Delhi is {observation}. Based on that, no umbrella needed today."
+        final_response = f"The weather in Hyderabad is {observation}. Based on that, no umbrella needed today."
         print("Response:", final_response)
 
-react_agent("What’s the weather in Delhi?")
+react_agent("What’s the weather in Hyderabad?")
